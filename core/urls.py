a30d2387afcf6f62e086b55f01ed97645eac9898
renato lourenço de moura <renatonec@ssp-go.sspj.go.gov.br>
2024-08-28 16:29:01 -0300

    Implementado a def login_view no arquivo views.py da aplicação core

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import index, principal, contato
+from .views import index, principal
 
 # Abaixo configure todas as rotas de urls, não utilizar o arquivo urls.py que fica dentro da aplicação CORE
 urlpatterns = [
     path('', index, name='index'),
     path('principal/', principal, name='principal'),
-    path('contato/', contato, name='contato'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html')
@@ -8,3 +10,17 @@ def principal(request):
 
 def contato(request):
     return render(request, 'contato.html')
+
+def login_view(request):
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(request, username=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('principal')  # Uma vez logado com sucesso, redireciona para a página principal
+        else:
+            return HttpResponse("A tentativa de login falhou, verifique os dados digitados e tente novamente.")
+        
+    return render(request, 'index.html')
